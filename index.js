866908b0c507e725e9293dced86ce62dc5a9e63d
Aniket Goyal <129101494+Aniket110903@users.noreply.github.com>
2024-06-07 02:39:29 +0530

    Bug FIx Decimal

--- index.js
+++ index.js
@@ -8,9 +8,9 @@ const homeFile = fs.readFileSync("home.html", "utf-8");
 
 
 const replaceVal = (tempVal, orgVal) => {
-  let temperature = tempVal.replace("{%tempval%}", orgVal.main.temp-273.15);
-  temperature = temperature.replace("{%tempmin%}", orgVal.main.temp_min-273.15);
-  temperature = temperature.replace("{%tempmax%}", orgVal.main.temp_max-273.15);
+  let temperature = tempVal.replace("{%tempval%}", (orgVal.main.temp-273.15).toFixed(1));
+  temperature = temperature.replace("{%tempmin%}", (orgVal.main.temp_min-273.15).toFixed(1));
+  temperature = temperature.replace("{%tempmax%}", (orgVal.main.temp_max-273.15).toFixed(1));
   temperature = temperature.replace("{%location%}", orgVal.name);
   temperature = temperature.replace("{%country%}", orgVal.sys.country);
   temperature = temperature.replace("{%tempstatus%}", orgVal.weather[0].main);
